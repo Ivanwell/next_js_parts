5fa774bd463f83e32e5a505e44865b0f4baf825e
Ivanwell <lviv08@gmail.com>
2024-01-31 16:49:58 +0200

    updated promo image

--- pages/index.js
+++ pages/index.js
@@ -118,6 +118,9 @@ const NewMainPage = ({ userAgent }) => {
     }
   }, [])
 
+  const arrow = '<'
+  const arrowRew = '>'
+
   return (
     <div className={styles.main_container}>
       <Head>
@@ -222,7 +225,14 @@ const NewMainPage = ({ userAgent }) => {
           </Link>
         </form>
         <div className={styles.promo_container}>
-          <img src="https://backend.bayrakparts.com/images/media/%D0%9F%D1%96%D0%B4%D0%B3%D0%BE%D1%82%D1%83%D0%B9%D1%82%D0%B5%20%D1%81%D0%B2%D0%BE%D1%94%20%D0%B0%D0%B2%D1%82%D0%BE%20%D0%B4%D0%BE%20%D0%B7%D0%B8%D0%BC%D0%B8%20%D1%80%D0%B0%D0%B7%D0%BE%D0%BC%20%D0%B7%20%D0%BD%D0%B0%D0%BC%D0%B8%20(1).jpg" />
+          <div className={styles.arrow_cont}>{arrow}</div>
+          <div className={styles.imag_contaier}>
+            <img
+              loading="lazy"
+              src="https://backend.bayrakparts.com/images/media/baner_new.jpg"
+            />
+          </div>
+          <div className={styles.arrow_cont}>{arrowRew}</div>
         </div>
       </div>
       <div className={styles.container_for_brands}>
