a4f46e23f8e63a228bb86e13c002d65b5a930d9f
Ivanwell <lviv08@gmail.com>
2024-05-08 16:02:38 +0300

    changed order function

--- pages/checkout.js
+++ pages/checkout.js
@@ -198,7 +198,9 @@ const CheckOut = () => {
       },
     })
     fetch(
-      `https://api.telegram.org/bot6173056848:AAE0eviFsiQtx0CWxEJyBizEdl_zhaJ0P1w/sendMessage?chat_id=@edetalRequests&text=Нове замовлення ${orderNumber} BayrakParts! ${
+      `https://api.telegram.org/bot6173056848:${
+        process.env.TOKEN_TELEGRAM
+      }/sendMessage?chat_id=@edetalRequests&text=Нове замовлення ${orderNumber} BayrakParts! ${
         city + ' ' + department + ' ' + pib + ' ' + phone + ' ' + articlesToSend
       }`
     )
